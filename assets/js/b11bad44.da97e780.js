2e2d8cabb79a797ece027c8ca3f3a884963885e4
NENmSIS <nenmsis@protonmail.com>
2022-01-03 15:12:53 +0100

    Deploy website - based on 996f68a5ea78d9ae7f63f53676786d52fceb9f0d

---
+++ assets/js/b11bad44.da97e780.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[221],{2030:function(s){s.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/news","name":"News","count":1,"permalink":"/blog/tags/news"}')}}]);
